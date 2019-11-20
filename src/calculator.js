ff32a1e9aee727adff691d59bee93da534276b92
Knarhi <kuisma.narhi@student.lut.fi>
2019-11-21 01:36:51 +0200

    tyylipohdintaa

--- src/calculator.js
+++ src/calculator.js
@@ -1,27 +1,28 @@
+"use strict";
 class Calculator {
 
-    constructor(gender, weight, timeOfStarting, portions) {
+    constructor(gender, weightInKg, timeOfStarting, portions) {
         this.man = 0
         this.woman = 1
-        this.userSex = gender;
-        this.userWeightInKg = weight;
+        this.userWeightInKg = weightInKg;
         this.timeOfStarting = timeOfStarting;
         this.numberOfPortions = portions;
         this.currentTime = new Date();
         this.hoursFromStarting = Math.abs(this.currentTime - this.timeOfStarting) / 3600000;
-        if (this.userSex === man) {
+        if (gender === this.man) {
             this.genderBurnFactor = 0.013;
             this.genderIntoxicationFactor = 16;
         } else  {
             this.genderBurnFactor = 0.015;
             this.genderIntoxicationFactor = 18.18;
         }
-        this.burnRatePerHour = this.genderBurnFactor * this.userWeight;
+        this.burnRatePerHour = this.genderBurnFactor * this.userWeightInKg;
+        this.intoxicationAtZerHour = this.genderIntoxicationFactor / this.userWeightInKg * this.numberOfPortions;
     }
 
-    calculateCurrentIntoxication(){
-        var intoxicationAtZerHour = this.genderIntoxicationFactor / this.userWeightInKg * this.numberOfPortions;
-        var currentIntoxication = intoxicationAtZerHour - (this.burnRatePerHour * this.hoursFromStarting);
+    this.calculateCurrentIntoxication(){
+
+        var currentIntoxication = this.intoxicationAtZerHour - (this.burnRatePerHour * this.hoursFromStarting);
 
         if (currentIntoxication > 0) {
             return Math.round(currentIntoxication * 100) / 100;
@@ -30,7 +31,7 @@ class Calculator {
         }
     }
 
-    calculteTimeUntilSober(currentIntoxication) {
+    this.calculteTimeUntilSober(currentIntoxication) {
         if (currentIntoxication > 0) {
             return Math.round((currentIntoxication / this.burnRatePerHour));
         } else {
@@ -40,8 +41,8 @@ class Calculator {
 
     this.giveResultParagraph() {
         var currentIntoxication = calculateCurrentIntoxication();
-        var timeUntilSober = calculateCurrentIntoxication(currentIntoxication);
-        var paragraph = "<p id=result>", currentIntoxication, "promillen känni päällä, ", timeUntilSober," tuntia selviämiseen.</p>"
-        return([currentIntoxication,timeuntilSober]);
+        var timeUntilSober = calculteTimeUntilSober(currentIntoxication);
+        var paragraph = "<p id=result>" currentIntoxication.toString() +  " promillen känni päällä, " + timeUntilSober.toString() + " tuntia selviämiseen.</p>"
+        return(paragraph);
     }
 }
